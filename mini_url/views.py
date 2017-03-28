406919dec14eef9aacc5a2f994773c52cc87de90
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-03-28 14:12:07 +0200

    Update views.py

--- mini_url/views.py
+++ mini_url/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import MiniUrl
-from .forms import MiniUrlForm, ContactForm
+from .forms import MiniUrlForm
 from django.views.generic.edit import FormView
 from django.views.generic.base import TemplateView
 from django.core.mail import EmailMessage, mail_admins#, send_email
@@ -40,36 +40,3 @@ def redirection(request, code):
 
     return redirect(mini.url, permanent=True)
 
-
-def contact_us_view(request):
-    form_class = ContactForm
-
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get('contact_name', '')
-            contact_email = request.POST.get('contact_email', '')
-            form_content = request.POST.get('content', '')
-
-            template = get_template('contact_template.txt')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content
-            })
-
-            content = template.render(context)
-
-            email = EmailMessage(
-                "Nouveau message de Shorten app",
-                content,
-                "Shorten app" + '',
-                ['delita.makanda@gmail.com'],
-                headers = { 'Reply-To': contact_email }
-            )
-
-            email.send()
-            return redirect('/contact')
-
-    return render(request, 'mini_url/contact.html', {'form': form_class})
