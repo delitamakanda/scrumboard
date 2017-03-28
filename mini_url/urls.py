40c1e60fe578b969e1844d194e4a8db21aff3385
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-03-28 13:54:54 +0200

    Update urls.py

--- mini_url/urls.py
+++ mini_url/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^urls/$', views.liste, name='liste'),
     url(r'^$', views.nouveau, name='nouveau'),
     url(r'^(?P<code>\w{6})/$', views.redirection, name='redirection'),
-    url(r'^urls/success/([0-9]+)/$', views.success_miniurl, name='success_miniurl'),
+    url(r'^urls/success/(?P<pk>\d+)/$', views.success_miniurl, name='success_miniurl'),
     url(r'^contact/$', views.contact_us_view, name="contact"),
     url(r'^privacy-policies/$', TemplateView.as_view(template_name='mini_url/privacy-policies.html'), name="privacy-policies"),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
