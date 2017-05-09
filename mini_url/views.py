03f77f41105df37363314e1fc2d998e739265e9d
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-09 10:23:19 +0200

    add bot

--- mini_url/views.py
+++ mini_url/views.py
@@ -1,3 +1,8 @@
+import json, requests, random, re
+
+from pprint import pprint
+from django.views import generic
+from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import MiniUrl
 from .forms import MiniUrlForm
@@ -12,6 +17,38 @@ from django.contrib import messages
 import datetime
 from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+verify_token = "my_voice_is_my_password"
+
+
+def get_joke(fbid, recevied_message):
+    joke_text = requests.get("http://api.icndb.com/jokes/random/").json()['value']['joke']
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%'EAAYhpiOyWHsBAGXoYf6H6OigfpnPuZBwtGVQEoEGucqZA2ZBlRAPTfJ3KYGqaZBlhh82oV7jQgLkXPUZAGqFS9d0FfW5X0Wkbj6ZCFuo4sa8FmQZBZBPMXAZCaqgracbeHFNxk2vOBKlfN2mvUxc7hk6plLZBxuFVUxCZAE4BW8qOGYVwZDZD'
+    response_msg = json.dumps({"recipient": {"id": fbid }, "message": {"text": joke_text}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    pprint(status.json())
+
+class jokebot(generic.View):
+    def get(self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == verify_token:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error')
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+
+    def post(self, request, *args, **kwargs):
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                if 'message' in message:
+                    get_joke(message['sender']['id'], message['message']['text'])
+        return HttpResponse()
 
 def liste(request):
     minis_list = MiniUrl.objects.order_by('-nb_acces')
