efbf327a1c31d1fd9cb643e95fb278314d8c8e13
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-18 17:27:52 +0200

    Update app.js

--- mini_url/static/scripts/app.js
+++ mini_url/static/scripts/app.js
@@ -1,13 +1,2 @@
 $(document).foundation()
 
-$(function() {
-  var infinite = new Waypoint.Infinite({
-      element: $('.infinite-container')[0],
-      onBeforePageLoad: function () {
-        $('.loading').show();
-      },
-      onAfterPageLoad: function ($items) {
-        $('.loading').hide();
-      }
-    });
-});
