4b0684d93e3c366b5610a7e4e09ce7f736ee09e4
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-23 11:50:35 +0200

    Update forms.py

--- mini_url/forms.py
+++ mini_url/forms.py
@@ -16,7 +16,8 @@ class SignupForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Must be a valid email address.')
+    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
     
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+        fields = ('username', 'birth_date', 'email', 'password1', 'password2',)
