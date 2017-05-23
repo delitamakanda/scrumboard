fd90a2e760aaeaef96a6507939d241c19f10f389
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-23 11:49:12 +0200

    Update models.py

--- mini_url/models.py
+++ mini_url/models.py
@@ -3,8 +3,23 @@ import random
 import string
 from urllib2 import urlopen
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
 class MiniUrl(models.Model):
     url = models.URLField(verbose_name="URL a reduire", unique=True)
     code = models.CharField(max_length=6, unique=True)
