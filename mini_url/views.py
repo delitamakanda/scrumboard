444567e6eb9029a65a91f06fee53ece02db1ae65
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-23 12:14:38 +0200

    Update views.py

--- mini_url/views.py
+++ mini_url/views.py
@@ -104,14 +104,25 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user = form.save(commit=False)
+            user.is_active = False
+            #user.refresh_from_db()
+            #user.profile.birth_date = form.cleaned_data.get('birth_date')
             user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('liste')
+            current_site = get_current_site(request)
+            subject = 'Activer votre compte'
+            message = render_to_string('mini_url/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            #raw_password = form.cleaned_data.get('password1')
+            #user = authenticate(username=username, password=raw_password)
+            #login(request, user)
+            #return redirect('liste')
+            return redirect('account_activation_sent')
     else:
         form = SignupForm()
     return render(request, 'mini_url/signup.html', {'form': form})
