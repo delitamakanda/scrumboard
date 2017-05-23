de49bdb57a6891d7c60a39fe0ddc37aceea6c78b
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-23 12:24:27 +0200

    Update views.py

--- mini_url/views.py
+++ mini_url/views.py
@@ -22,7 +22,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
@@ -127,3 +127,28 @@ def signup(request):
         form = SignupForm()
     return render(request, 'mini_url/signup.html', {'form': form})
 
+
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_encode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+        
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('liste')
+    else:
+        return render(request, 'mini_url/account_activation_invalid.html')
+    
+    
+ 
+        
+    
+    
+    
