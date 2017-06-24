99bb2e07fd9b7a2a23b8876b89a82290329af03d
Délita Makanda <delita.makanda@gmail.com>
2017-06-24 20:35:35 +0200

    add website

--- mini_url/models.py
+++ mini_url/models.py
@@ -8,6 +8,7 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
+    website = models.URLField(null=True, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     email_confirmed = models.BooleanField(default=False)
