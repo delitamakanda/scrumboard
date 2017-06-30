bb487797dbeedf3f0cff708fd1a5c49ff5de0f27
Délita Makanda <delita.makanda@gmail.com>
2017-06-30 22:41:00 +0200

    add update feature

--- mini_url/static/scrumboard/card.directive.js
+++ mini_url/static/scrumboard/card.directive.js
@@ -7,7 +7,16 @@
     function CardDirective() {
         return {
             templateUrl: '/static/scrumboard/card.html',
-            restrict: 'E'
+            restrict: 'E',
+            controller: ['$scope', '$http', function($scope, $http) {
+                var url = 'scrumboard/cards/' + $scope.card.id + '/';
+                $scope.update = function() {
+                    $http.put(
+                        url,
+                        $scope.card
+                    );
+                };
+            }]
         };
     }
 })();
