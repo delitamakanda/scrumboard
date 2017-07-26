44e334469d8f289e4c6c1cbf6cf1ee6d3a7083cb
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-07-26 15:20:47 +0200

    Update signup.controller.js

--- url_shortener/static/scripts/signup.controller.js
+++ url_shortener/static/scripts/signup.controller.js
@@ -1,1 +1,27 @@
+(function() {
+    'use strict';
 
+    angular
+        .module('scrumboard')
+        .controller('SignupController', ['$scope', '$http', '$location', SignupController]);
+
+    function SignupController($scope, $http, $location) {
+        
+        $scope.signup = function() {
+
+            var data = {
+                username: $scope.username,
+                password: $scope.password
+            };
+
+            $http.post('/auth_api/register/', data)
+                .then(function(response) {
+                    $location.url('/login');
+                }, function(){
+                    //console.log('error');
+                    $scope.signup_error="An error occurred or username already taken.";
+                });
+
+        }
+    }
+})();
