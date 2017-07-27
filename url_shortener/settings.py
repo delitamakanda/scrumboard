180e395c90b8fdf544f20e704fac379e9bdd05a0
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-07-27 16:48:26 +0200

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'url_shortener.profile_middleware',
 ]
 
 ROOT_URLCONF = 'url_shortener.urls'
