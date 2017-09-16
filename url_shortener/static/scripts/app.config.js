3471751675b67eefe7ba15e3b4e04dd1c647d85c
Délita Makanda <delita.makanda@gmail.com>
2017-09-16 19:28:33 +0200

    add popup

--- url_shortener/static/scripts/app.config.js
+++ url_shortener/static/scripts/app.config.js
@@ -20,10 +20,6 @@
                 templateUrl: '/static/components/signup.html',
                 controller: 'SignupController'
             })
-            .when('/add-list', {
-                templateUrl: '/static/components/add.html',
-                controller: 'ScrumboardController'
-            })
             .otherwise('/')
     }
 
