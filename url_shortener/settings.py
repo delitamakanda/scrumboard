10574a8b28adcd6cd87169a6aa044ea5f402dc5f
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-09-21 12:46:44 +0200

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cye+m6#rw184d%q0iasyu19!f73&y8e77yd2eb^z82r=p^sz0_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
