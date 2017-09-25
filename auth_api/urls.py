2b512ee73e73e13342e2f3005b629355dc77bf57
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-09-25 10:06:36 +0200

    Update urls.py

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .api import LoginView, LogoutView, SignupView, CheckoutUser
+from .api import LoginView, LogoutView, SignupView, CheckoutUser, GetCurrentUser
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view()),
     url(r'^logout/$', LogoutView.as_view()),
     url(r'^register/$', SignupView.as_view()),
     url(r'^check_login/$', CheckoutUser.as_view()),
+    url(r'^current_user/$', GetCurrentUser.as_view()),
 ]
