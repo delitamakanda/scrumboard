d2f933ad472ba62e56903d6c876bdc4bb04b2019
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-09-29 14:01:13 +0200

    Update permissions.py

--- mini_url/permissions.py
+++ mini_url/permissions.py
@@ -6,3 +6,16 @@ class IsUserOfPost(permissions.BasePermission):
         if request.user:
             return list.user == request.user
         return False
+    
+    
+    
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.owner == request.user
