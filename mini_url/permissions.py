8636b3a6cbd79c930f474d7284bfaf4869641146
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-11-27 16:40:18 +0100

    Update permissions.py

--- mini_url/permissions.py
+++ mini_url/permissions.py
@@ -9,19 +9,6 @@ class IsUserOfPost(permissions.BasePermission):
     
     
     
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-    
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
-        return obj.owner == request.user
-    
-    
-    
 class IsAdminUser(permissions.BasePermission):
     """
     Allows access only to admin users.
