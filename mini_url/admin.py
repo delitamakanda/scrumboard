292782f12f8afc28cc87c67b75ab64a327104622
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-12-11 17:06:36 +0100

    fix exchange class

--- mini_url/admin.py
+++ mini_url/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import List, Card, Todo
 
 # Register your models here.
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ['user', 'name']
-    
 @admin.register(List)
 class ListAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name']
+    
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
     list_display = ['name', 'user', 'created']
 
 admin.site.register(Card)
