6e0b0b63eba4467c719c395113edab1439165a17
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-12-15 15:32:38 +0100

    duplicate controller fix

--- url_shortener/static/scripts/todo.controller.js
+++ url_shortener/static/scripts/todo.controller.js
@@ -1,40 +1,40 @@
 (function(){
     'use strict';
 
-    angular.module('scrumboard')
-        .controller('TodoController', TodoController,
-        ['$scope', '$http', '$location', '$window', 'Login', TodoController]);
+    angular
+        .module('scrumboard')
+        .controller('TodoController', ['$scope', '$http', '$location', '$window', 'Login', TodoController]);
 
-        function TodoController($scope, $http, $location, $window, Login){
-            
-            $scope.createTask = function() {
-                var todo = {
-                    name: $scope.name,
-                    text: $scope.text,
-                };
-                
-                $http.post('/scrumboard/todos/', todo)
-                    .then(function(response) {
-                        $location.url('/todo');
-                        $window.location.reload();
-                    }, function(){
-                        console.log('error');
-                    });
+    function TodoController($scope, $http, $location, $window, Login){
+
+        $scope.createTask = function() {
+            var todo = {
+                name: $scope.name,
+                text: $scope.text,
             };
-            
-            // modelOptions
-            $scope.modelOptions = {
-                debounce: 500
-            };
-            
-            Login.redirectedIfNotLoggedIn();
-            $scope.data = [];
 
-            // fetch all of your lists and cards
-            $http.get('/scrumboard/todos').then(function(response){
-                $scope.data = response.data;
-                console.log($scope.data);
-            });
+            $http.post('/scrumboard/todos/', todo)
+                .then(function(response) {
+                    $location.url('/todo');
+                    $window.location.reload();
+                }, function(){
+                    console.log('error');
+                });
+        };
 
-        }
+        // modelOptions
+        $scope.modelOptions = {
+            debounce: 500
+        };
+
+        Login.redirectedIfNotLoggedIn();
+        $scope.data = [];
+
+        // fetch all of your lists and cards
+        $http.get('/scrumboard/todos').then(function(response){
+            $scope.data = response.data;
+            console.log($scope.data);
+        });
+
+    }
 }());
