b63a489bec9702e356fc0d725002d5bfb0b13015
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-08 14:10:44 +0200

    Update gulp.config.js

--- gulp/gulp.config.js
+++ gulp/gulp.config.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  build_dir: 'build',
+  app_files: {
+    js: ['src/**/*.js'],
+    tpl_src:['./build/vendor/**/*.js',
+  './build/app/**/*.js',
+  './build/assets/css/**/*.css']
+  }
+};
