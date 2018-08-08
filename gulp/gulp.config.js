5f16eff1ce38ba9bb5e90a3cd3c3c5e10fc93570
Délita Makanda <delita.makanda@gmail.com>
2018-08-08 22:46:38 +0200

    chore refactor

--- gulp/gulp.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  build_dir: 'build',
-  app_files: {
-    js: ['src/**/*.js'],
-    tpl_src:['./build/vendor/**/*.js',
-  './build/app/**/*.js',
-  './build/assets/css/**/*.css']
-  }
-};
