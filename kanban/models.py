0acbaadd12a47a9d7faf6b9235e1cc76896ef49a
Délita Makanda <delitamakanda@users.noreply.github.com>
2022-03-22 14:31:17 +0100

    fix: on_delete

--- kanban/models.py
+++ kanban/models.py
@@ -27,7 +27,7 @@ class Card(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     extra = models.CharField(max_length=50, blank=True, null=True)
-    list = models.ForeignKey(List, related_name='cards')
+    list = models.ForeignKey(List, related_name='cards', on_delete=models.CASCADE)
     story_points = models.IntegerField(null=True, blank=True)
     business_value = models.IntegerField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
