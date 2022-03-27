1c14eaab5d664c0f597e6c956b5117c3618cac8b
Délita Makanda <delita.makanda@gmail.com>
2022-03-28 00:34:16 +0200

    up to django 2.2

--- kanban/urls.py
+++ kanban/urls.py
@@ -2,9 +2,9 @@ from .api import ListViewSet, CardViewSet, UsersViewsSet, TodoViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'lists', ListViewSet, base_name='lists')
-router.register(r'cards', CardViewSet, base_name='cards')
-router.register(r'users', UsersViewsSet, base_name='users')
-router.register(r'todos', TodoViewSet, base_name='todos')
+router.register(r'lists', ListViewSet, basename='lists')
+router.register(r'cards', CardViewSet, basename='cards')
+router.register(r'users', UsersViewsSet, basename='users')
+router.register(r'todos', TodoViewSet, basename='todos')
 
 urlpatterns = router.urls
--- scrumboard/settings.py
+++ scrumboard/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.i18n',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
